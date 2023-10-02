fc87706abed1388999211b9347d163ed7b2bcaf8
Etson <etsond@ufl.edu>
2023-10-02 16:19:28 -0400

    fixing features

--- main.js
+++ main.js
@@ -1,160 +1,124 @@
-// Initialize Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAFN289x3r01LRFatbuWLQE67YqPjNbbKM",
-  authDomain: "trackease-4f595.firebaseapp.com",
-  projectId: "trackease-4f595",
-  storageBucket: "trackease-4f595.appspot.com",
-  messagingSenderId: "258159948655",
-  appId: "1:258159948655:web:59774eb75f85e296f22e9a",
-//   measurementId: "G-JD21GHZRJN"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Function to execute after successful sign-in
-function onSignIn(user) {
-  console.log('Logged in user:', user.displayName);
-  // You can redirect or update the UI as needed.
-}
-
-// Add a click event listener to the Google Sign-In button
-const googleSignInBtn = document.getElementById('google-signin-btn');
-
-googleSignInBtn.addEventListener('click', () => {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then((result) => {
-      const user = result.user;
-      // Handle successful sign-in here
-    })
-    .catch((error) => {
-      console.error('Google Sign-In Error:', error);
-    });
-});
-
-// Function to execute after successful sign-in
-function onSignIn(user) {
-  console.log('Logged in user:', user.displayName);
-
-  // Update user profile information
-  document.getElementById('user-photo').src = user.photoURL || 'default-user-photo.jpg';
-  document.getElementById('user-name').textContent = user.displayName;
-  document.getElementById('user-email').textContent = user.email;
-
-}
+document.getElementById('issueInputForm').addEventListener('submit',saveIssue)
 
 
 
-
-document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
-
-// Retrieve the issues from localStorage and display them on the page
+// retrieve the  issues from localstorage and display it on page
 function fetchIssues() {
-  let issues = JSON.parse(localStorage.getItem('issues'))
-  let issuesList = document.getElementById('issuesList')
+    let issues = JSON.parse(localStorage.getItem('issues'))
+    let issuesList = document.getElementById('issuesList')
 
-  issuesList.innerHTML = '';
+    issuesList.innerHTML = '';
+// iterates over the getIssues 
+    for (let i = 0; i < issues.length; i++) {
+        let id = issues[i].id
+        let subject = issues[i].subject
+        let description = issues[i].description
+        let severity = issues[i].severity
+        let assignedTo = issues[i].assignedTo
+        let status = issues[i].status
+        let statusColor = status == "Closed" ? 'label-success' : 'label-info'
 
-  if (issues === null || issues === undefined) {
-    return;
-  }
-
-  for (let i = 0; i < issues.length; i++) {
-    let id = issues[i].id;
-    let subject = issues[i].subject;
-    let description = issues[i].description;
-    let severity = issues[i].severity;
-    let assignedTo = issues[i].assignedTo;
-    let status = issues[i].status;
-    let statusColor = status == 'Closed' ? 'label-success' : 'label-info';
-
-    issuesList.innerHTML +=
-      '<div class="well">' +
-      '<h6>Issue ID:' + id + '</h6>' +
-      '<p><span class= "label ' + statusColor + ' ">' + status + '</span></p>' +
-      '<h3>' + subject + '</h3>' +
-      '<p>' + description + '</p>' +
-      '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' ' + '<span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>' +
-      '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\'' + id + '\')">Close</a> ' +
-      '<a href="#" class="btn btn-danger" onclick="deleteIssue(\'' + id + '\')">Delete</a> ' +
-      '<a href="#" class="btn btn-primary" onclick="editIssue(\'' + id + '\')">Edit</a>'
-      + '</div>';
-  }
+        // add the issues to the html element
+        issuesList.innerHTML += 
+        '<div class="well">' +
+        '<h6>Issue ID:' + id + '</h6>' +
+        '<p><span class= "label ' + statusColor + ' ">' + status + '</span></p>' +
+        '<h3>' + subject + '</h3>' +
+        '<p>' + description + '</p>' + 
+        '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' ' + '<span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>' +
+        '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\''+id+'\')">Close</a> ' +
+        '<a href="#" class="btn btn-danger" onclick="deleteIssue(\''+id+'\')">Delete</a> ' +
+        '<a href="#" class="btn btn-primary" onclick="editIssue(\''+id+'\')">Edit</a>'
+        + '</div>'
+    }
 }
 
-// Save a new issue to localStorage and fetch & display issues on the page
+// save a new localstorage and fecth & display issue on page
+// first grab the value of issue sub,description,severity and assigned to filed
 function saveIssue(e) {
-  let issueId = chance.guid();
-  let issueSubject = document.getElementById('issueSubjInput').value;
-  let issueDesc = document.getElementById('issueDescInput').value;
-  let issueSeverity = document.getElementById('issueSeverityInput').value;
-  let issueAssignedTo = document.getElementById('issueAssignedToInput').value;
-  let issueStatus = 'Open';
+    // generate new ID
+    let issueId = chance.guid()
+    let issueSubject = document.getElementById('issueSubjInput').value
+    let issueDesc = document.getElementById('issueDescInput').value
+    let issueSeverity = document.getElementById('issueSeverityInput').value
+    let issueAssignedTo = document.getElementById('issueAssignedToInput').value
+    let issueStatus = 'Open'
 
-  let issue = {
-    id: issueId,
-    subject: issueSubject,
-    description: issueDesc,
-    severity: issueSeverity,
-    assignedTo: issueAssignedTo,
-    status: issueStatus
-  }
+    // issue obj then push into issue arr stored in local storage
+    let issue = {
+        id: issueId,
+        subject: issueSubject,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+// if no issue arr is found, new arr is created and the new issue push into it
+    if(localStorage.getItem('issues')===null) {
+        let issues = []
+        issues.push(issue)
+        localStorage.setItem('issues', JSON.stringify(issues))
+    } else {
+        let issues = JSON.parse(localStorage.getItem('issues'))
+        issues.push(issue)
+        localStorage.setItem('issues', JSON.stringify(issues))
+    }
 
-  if (localStorage.getItem('issues') === null) {
-    let issues = [];
-    issues.push(issue);
-    localStorage.setItem('issues', JSON.stringify(issues));
-  } else {
-    let issues = JSON.parse(localStorage.getItem('issues'));
-    issues.push(issue);
-    localStorage.setItem('issues', JSON.stringify(issues));
-  }
+    // input field is reset
+    document.getElementById('issueInputForm').reset();
 
-  document.getElementById('issueInputForm').reset();
-  fetchIssues();
-  e.preventDefault();
+    // calling fect to display all issues on page
+    fetchIssues()
+// prevent form from being submitted and page reloading
+    e.preventDefault()
 }
 
+// allow user to edit an issue
 function editIssue(id) {
-  let issues = JSON.parse(localStorage.getItem('issues'));
-  let issueIndex = issues.findIndex(issue => issue.id === id);
+    // get the issue from arr from local storage
+    let issues = JSON.parse(localStorage.getItem('issues'))
+    // find the indexof the issue with respect to its ID
+    let issueIndex = issues.findIndex(issue => issue.id === id)
 
-  document.getElementById('issueSubjInput').value = issues[issueIndex].subject;
-  document.getElementById('issueDescInput').value = issues[issueIndex].description;
-  document.getElementById('issueSeverityInput').value = issues[issueIndex].severity;
-  document.getElementById('issueAssignedToInput').value = issues[issueIndex].assignedTo;
+    // updating the values of the input files with the current details
+    document.getElementById('issueSubjInput').value = issues[issueIndex].subject
+    document.getElementById('issueDescInput').value = issues[issueIndex].description
+    document.getElementById('issueSeverityInput').value = issues[issueIndex].severity
+    document.getElementById('issueAssignedToInput').value = issues[issueIndex].assignedTo
 
-  issues.splice(issueIndex, 1);
-  localStorage.setItem('issues', JSON.stringify(issues));
-  fetchIssues();
+    // remo issue from issues arr
+    issues.splice(issueIndex, 1)
+    // save the updated issue arr to local storage
+    localStorage.setItem('issues', JSON.stringify(issues))
+    // call fetch issues function to update page
+    fetchIssues()
 }
 
+// allowed user to mark issue as closed
 function setStatusClosed(id) {
-  let issues = JSON.parse(localStorage.getItem('issues'));
-  for (let i = 0; i < issues.length; i++) {
-    if (issues[i].id === id) {
-      issues[i].status = 'Closed';
+    let issues = JSON.parse(localStorage.getItem('issues'))
+    for(let i=0; i < issues.length; i++) {
+        if(issues[i].id === id) {
+            issues[i].status = "Closed"
+        }
     }
-  }
 
-  localStorage.setItem('issues', JSON.stringify(issues));
-  fetchIssues();
+    localStorage.setItem('issues', JSON.stringify(issues))
+
+    fetchIssues()
 }
 
-function deleteIssue(id) {
-  let issues = JSON.parse(localStorage.getItem('issues'));
-  for (let i = 0; i < issues.length; i++) {
-    if (issues[i].id === id) {
-      issues.splice(i, 1);
+// allow user to delete issue
+function deleteIssue (id) {
+    let issues = JSON.parse(localStorage.getItem('issues'))
+    for(let i=0; i < issues.length; i++) {
+        if(issues[i].id === id) {
+            issues.splice(i,1)
+        }
     }
-  }
 
-  localStorage.setItem('issues', JSON.stringify(issues));
-  fetchIssues();
+    localStorage.setItem('issues', JSON.stringify(issues))
+
+    fetchIssues()
+
 }
-
-// Initial fetch of issues when the page loads
-fetchIssues();
